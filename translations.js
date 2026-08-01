cbbcdb28be8c8fa42024b4811a3ed9f432e9bac5
puneeth <kpuneethkumar450@gmail.com>
2026-08-01 15:49:20 +0530

    Add GitHub social link with translations

--- translations.js
+++ translations.js
@@ -153,6 +153,7 @@ window.TRANSLATIONS = {
     "a11y.linkedin": "领英主页",
     "a11y.instagram": "Instagram 主页",
     "a11y.whatsapp": "WhatsApp 联系",
+    "a11y.github": "GitHub 主页",
     "a11y.achievements": "核心成果"
   }
 };
